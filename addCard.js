90243a8384541b8dea6e2e6fb2eb33189b380737
doeswayneexist <doeswayneeexist@gmail.com>
2013-04-13 23:13:26 -0700

    adds the init js files fo adding cards

---
+++ addCard.js
@@ -0,0 +1,10 @@
+// The function that adds the card to the database.
+var addCard = function () {
+
+	
+}
+// Check to see that the new card is a good card.
+
+// Pass the new card to a php page that will store the card.
+
+// Give the option to add another new card. 
