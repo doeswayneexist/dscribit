6fe697172b4d59ad55569e06f3bff84a0c77d9f6
doeswayneexist <doeswayneeexist@gmail.com>
2013-04-15 02:23:48 -0700

    implements modular db info

--- add_card.php
+++ add_card.php
@@ -1,14 +1,7 @@
 <?php
 
-$host="localhost"; // Host name 
-$username="root"; // Mysql username 
-$password="root"; // Mysql password 
-$db_name="dscribit"; // Database name 
-$tbl_name="cards"; // Table name 
-
-// Connect to server and select database.
-mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
-mysql_select_db("$db_name")or die("cannot select DB");
+//connect to databse database info
+require 'db_connect.php';
 
 // Get values from new_card page
 $word=$_POST['word'];
