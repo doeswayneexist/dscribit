4542f9778da64bfadd98e714f0ac053274ccb5f4
doeswayneexist <doeswayneeexist@gmail.com>
2013-04-16 12:48:13 -0700

    changes the help tool tips

--- new_card.php
+++ new_card.php
@@ -4,8 +4,9 @@ This page is used to create new cards.
 The user enters words into the form and an ajax function is called to pass the cards to a php page which updates the db
 */
 
-$step1 = "This is the word that will be guessed at. We suggest picking picking some object in the world like a basketball";
-$step2 = "This is a word that cannot be used in the explanation of the word in the green box. If the word is basketball, then a good word to ban would be court."
+$step1 = "Type in a word that you would like others to have to try to guess. We suggest picking picking some object in the world like a basketball";
+$step2 = "Words that you type in the red boxes cannot be used in the explanation of the word in the green box. If the word is basketball, then a good word to ban would be court.";
+$step3 = "Submit your card when you are ready to release your card to the word.";
 ?>
 
 <head>
@@ -26,11 +27,11 @@ $step2 = "This is a word that cannot be used in the explanation of the word in t
 		</div>
 		<input title="Type in a word." type="text" id="word" class="good" placeholder="Enter a word to be guessed." data-intro="<?php echo $step1 ?>" data-step='1'>
 		<input title="Type in a word." type="text" id="ban1" placeholder="Enter a banned word." data-intro="<?php echo $step2 ?>" data-step='2'>
-		<input title="Type in a word." type="text" id="ban2" placeholder="Enter a banned word." data-intro="<?php echo $step2 ?>" data-step='3'>
-		<input title="Type in a word." type="text" id="ban3" placeholder="Enter a banned word." data-intro="<?php echo $step2 ?>" data-step='4'>
-		<input title="Type in a word." type="text" id="ban4" placeholder="Enter a banned word." data-intro="<?php echo $step2 ?>" data-step='5'>
-		<input title="Type in a word." type="text" id="ban5" placeholder="Enter a banned word." data-intro="<?php echo $step2 ?>" data-step='6'>
-		<div class="submit" id="sub" onclick="addCard();">
+		<input title="Type in a word." type="text" id="ban2" placeholder="Enter a banned word.">
+		<input title="Type in a word." type="text" id="ban3" placeholder="Enter a banned word.">
+		<input title="Type in a word." type="text" id="ban4" placeholder="Enter a banned word.">
+		<input title="Type in a word." type="text" id="ban5" placeholder="Enter a banned word.">
+		<div class="submit" id="sub" onclick="addCard();" data-intro="<?php echo $step2 ?>" data-step='3'>
 			<span>Submit.</span>
 		</div>
 	</div>
